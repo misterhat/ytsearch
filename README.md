4a0d6ba2f6a868c3aeef0c382af935928778c2fe
misterhat <misterhat144@gmail.com>
2014-12-01 01:26:35 -0600

    added files

---
+++ README.md
@@ -0,0 +1,68 @@
+# ytsearch
+Search YouTube from the command-line without an API key. It can fetch URLs and
+various meta content in whatever output format you'd like. Out of the box it
+pairs well with ytdl.
+
+## Installation
+
+    # npm install -g ytsearch
+
+## Examples
+Display the first five searches for terms with title, descriptions and URLs:
+
+    $ ytsearch "dr katz" -f "%t\n%d\n%u\n\n" --limit 5
+
+Stream a TV show to mplayer:
+
+    $ ytdl "$(ytsearch "dr katz episode 2")" | mplayer -
+
+Find the link for a music track, stream the download to ffmpeg which strips the
+video, saves it to disk and finally streams the audio to mplayer:
+
+    $ ytdl "$(ytsearch altogether\ now --music)" | ffmpeg -i pipe:0 -vn -f mp3 \
+    altogether.mp3 -f mp3 pipe:1 | mplayer -
+
+## Manual
+```
+Usage: ytsearch <terms> [options]
+
+-h, --help
+        Display this screen.
+-p, --pretty
+        Sets the format to something pleasing to look at.
+        default: false
+-f, --format "%t %a %d %D %c %T %v %f %u %p"
+        Format a video's meta data with a specific format string.
+
+        %t - title
+        %a - author
+        %d - description
+        %D - duration
+        %c - category
+        %T - thumbnail
+        %v - views
+        %f - favorites
+        %u - url
+        %p - published
+
+        default: "%u\n"
+-l, --limit <number>
+        Limit the amount of videos returned by the specified number.
+        default: 1
+-o, --offset <number>
+        Offset the returned videos by the specified number.
+        default: 1
+-m, --music
+        Select the best music track for the specified terms. Note that this
+        flag ignores limit and offset if set as it only returns one video.
+        default: false
+--nostrip
+        Don't strip the backslashes from the format string.
+        default: false
+--inhuman
+        Don't humanize any of the meta information.
+        default: false
+```
+
+## License
+MIT
---
+++ ytsearch.js
@@ -0,0 +1,173 @@
+#!/usr/bin/env node
+var util = require('util'),
+
+    colors = require('colors'),
+    formatNumber = require('format-number')(),
+    minimist = require('minimist'),
+    musicVideo = require('youtube-best-video').findBestMusicVideo,
+    search = require('youtube-search'),
+    slashes = require('slashes'),
+    timelabel = require('timelabel'),
+    youtubeId = require('get-youtube-id'),
+
+    package = require('./package');
+
+var MINIHOST = 'https://youtu.be/%s',
+
+    FORMATS = {
+        '%t': 'title',
+        '%a': 'author',
+        '%d': 'description',
+        '%D': 'duration',
+        '%c': 'category',
+        '%T': 'thumbnail',
+        '%v': 'views',
+        '%f': 'favorites',
+        '%u': 'url',
+        '%p': 'published'
+    },
+
+    PRETTY =
+        colors.bold('%t') + ' (uploader: %a, views: ' + colors.green('%v') + ')\n' +
+        colors.bold('duration: ') + '%D\n' +
+        colors.bold('thumbnail: ') + '%T\n' +
+        colors.bold('description: ') + '%d\n' +
+        colors.bold('watch: ') + '%u\n\n',
+
+    FPCENT = String.fromCharCode(65285);
+
+var argv = minimist(process.argv.slice(2)),
+
+    terms = argv._[0] || '',
+
+    help = !!(argv.h || argv.help),
+
+    limit = +(argv.l || argv.limit) || 1,
+    offset = +(argv.o || argv.offset) || 1,
+    music = !!(argv.m || argv.music),
+    format = (argv.f || argv.format) || '%u\n',
+    pretty = (argv.p || argv.pretty),
+
+    nostrip = !!argv.nostrip,
+    inhuman = !!argv.inhuman;
+
+if (pretty) {
+    format = PRETTY;
+}
+
+if (!nostrip) {
+    format = slashes.strip(format);
+}
+
+function showHelp() {
+    var formats;
+
+    console.log(colors.bold(package.name + ' - ' + package.version));
+    console.log(package.description + '\n');
+
+    formats = Object.keys(FORMATS).reduce(function (list, symbol) {
+        return list + '\t' + symbol + ' - ' + FORMATS[symbol] + '\n';
+    }, '');
+
+    console.log([
+        'Usage: ytsearch <terms> [options]\n',
+
+        colors.bold('-h, --help'),
+        '\tDisplay this screen.',
+
+        colors.bold('-p, --pretty'),
+        '\tSets the format to something pleasing to look at.',
+        '\tdefault: ' + colors.underline('false'),
+
+        colors.bold('-f, --format') + ' ' +
+        colors.underline('"' + Object.keys(FORMATS).join(' ') + '"'),
+        '\tFormat a video\'s meta data with a specific format string.\n',
+        formats,
+        '\tdefault: ' + colors.underline('"%u\\n"'),
+
+        colors.bold('-l, --limit') + ' '  + colors.underline('<number>'),
+        '\tLimit the amount of videos returned by the specified number.',
+        '\tdefault: ' + colors.underline('1'),
+
+        colors.bold('-o, --offset') + ' ' + colors.underline('<number>'),
+        '\tOffset the returned videos by the specified number.',
+        '\tdefault: ' + colors.underline('1'),
+
+        colors.bold('-m, --music'),
+        '\tSelect the best music track for the specified terms. Note that this',
+        '\tflag ignores limit and offset if set as it only returns one video.',
+        '\tdefault: ' + colors.underline('false'),
+
+        colors.bold('--nostrip'),
+        '\tDon\'t strip the backslashes from the format string.',
+        '\tdefault: ' + colors.underline('false'),
+
+        colors.bold('--inhuman'),
+        '\tDon\'t humanize any of the meta information.',
+        '\tdefault: ' + colors.underline('false')
+    ].join('\n'));
+}
+
+function formatVideo(video) {
+    var display = format.slice();
+
+    // Replace "%" with full-width percents.
+    video.title = video.title.replace('%', FPCENT);
+    video.description = video.description.replace('%', FPCENT);
+
+    video.url = util.format(MINIHOST, youtubeId(video.url));
+
+    try {
+        video.thumbnail = video.thumbnails[0].url;
+    } catch (e) {
+        video.thumbnail = '';
+    }
+
+    video.views = +video.statistics.viewCount || 0;
+    video.favorites = +video.statistics.favouriteCount;
+
+
+    if (!inhuman) {
+        video.duration = timelabel((+video.duration * 1000) || 0, true);
+
+        video.views = formatNumber(video.views);
+        video.favorites = formatNumber(video.favorites);
+    }
+
+    Object.keys(FORMATS).forEach(function (symbol) {
+        if (format.indexOf(symbol) === -1) {
+            return;
+        }
+
+        display = display.replace(symbol, video[FORMATS[symbol]]);
+    });
+
+    return display;
+}
+
+if (help || !terms) {
+    return showHelp();
+}
+
+if (music) {
+    return musicVideo(terms, function (err, video) {
+        if (err) {
+            return console.error(err.stack);
+        }
+
+        process.stdout.write(formatVideo(video));
+    });
+}
+
+search(terms, {
+    startIndex: offset,
+    maxResults: limit
+}, function (err, videos) {
+    if (err) {
+        return console.error(err.stack);
+    }
+
+    videos.forEach(function (video) {
+        process.stdout.write(formatVideo(video));
+    });
+});
